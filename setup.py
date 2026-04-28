6b1d98b8e13f8d38218b9a20c0ede33273bc45d7
BMurph04 <bmurph@mit.edu>
2026-04-28 15:18:30 -0400

    updated setup.py file to match execution of previous packages

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
-import glob
 import os
-
-from setuptools import setup
-
+from glob import glob
+from setuptools import setup, find_packages
 
 package_name = 'final_challenge2026'
-
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
-    scripts=[
-        'scripts/homography_transformer.py',
-        'scripts/lane_detector_node.py',
-        'scripts/lane_follower_node.py'
-    ],
+    # Automatically finds the 'final_challenge2026' python module folder
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml', 'README.md']),
-        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.xml'))),
-        ('share/' + package_name + '/media', glob.glob(os.path.join('media', '*'))),
-        ('share/' + package_name + '/racetrack_images', glob.glob(os.path.join('racetrack_images', '*', '*.png'))),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.*')),
+        (os.path.join('share', package_name, 'media'), glob('media/*')),
+        (os.path.join('share', package_name, 'racetrack_images'), glob('racetrack_images/*/*.png')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,4 +23,10 @@ setup(
     maintainer_email='racecar@mit.edu',
     description='Final challenge package for the RSS racecar',
     license='MIT',
-)
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lane_detector = final_challenge2026.part_a.lane_detector_node:main',
+            'lane_follower = final_challenge2026.part_b.lane_follower_node:main',
+        ],
+    },
