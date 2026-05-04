f23d28af67994fe3927b2e83e188e0dfd4584723
ifenetangerine <irenet@mit.edu>
2026-05-03 22:09:29 -0400

    ch

--- final_challenge2026/part_a/lane_detector_node.py
+++ final_challenge2026/part_a/lane_detector_node.py
@@ -40,7 +40,7 @@ class LineDetector(Node):
         self.declare_parameter("low_threshold", 50)
         self.declare_parameter("high_threshold", 150)
         self.declare_parameter("direction", "left")
-        self.declare_parameter("horizon_y_ratio", 0.45)
+        self.declare_parameter("horizon_y_ratio", 0.485)
         self.declare_parameter("goal_topic", "/goal_point")
 
         self.debug_topic = self.get_parameter("debug_topic").value
