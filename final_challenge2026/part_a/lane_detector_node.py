c771bd4aef87c6ece75097c8ad318645aa6f53d2
ifenetangerine <irenet@mit.edu>
2026-05-03 22:10:04 -0400

    lane width

--- final_challenge2026/part_a/lane_detector_node.py
+++ final_challenge2026/part_a/lane_detector_node.py
@@ -61,7 +61,7 @@ class LineDetector(Node):
         self.last_right_line = None
         self.lane_width_sum_px = 0.0
         self.lane_width_count = 0
-        self.avg_lane_width_px = 235.0
+        self.avg_lane_width_px = 180.0
 
         # use this line to debug with static images:
         # self.load_and_publish_image('src/final_challenge2026/racetrack_images/lane_3/image45.png')
